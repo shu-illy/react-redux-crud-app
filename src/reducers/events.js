86970cf67342d671c7c7d4e7b26b85c1797e4c49
shu-illy <sh.illy.9117@gmail.com>
2021-12-30 10:52:59 +0900

    32

---
+++ src/reducers/events.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import { READ_EVENTS } from "../actions";
+
+const switchEvents = (events = {}, action) => {
+  switch (action.type) {
+    case READ_EVENTS:
+      return _.mapKeys(action.response.data, "id");
+    default:
+      return events;
+  }
+};
+
+export default switchEvents;
+
+// 下記でエラー解消
+// https://techtechmedia.com/cannnot-export-arrow-function/
