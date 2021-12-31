b000e542e1aa4736a25a08530c880fd688dcbf02
shu-illy <sh.illy.9117@gmail.com>
2021-12-31 17:55:26 +0900

    34-1

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 import reducer from "./reducers";
 import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
 import registerServiceWorker from "./reportWebVitals";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EventsIndex />} />
+        <Route path="/events/new" element={<EventsNew />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
